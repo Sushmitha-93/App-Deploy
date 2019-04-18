4208eec230afca9bab556f89d32bcef6b188d3bf
Sushmitha DT <dtsushmitha@gmail.com>
2019-04-19 00:36:58 +0530

    Updates

---
+++ precache-manifest.cf75d6c71b20e4f8c6449715dbdafc5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sushmitha-93/App-Deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cc39a64a6082735b92b6",
+    "url": "/Sushmitha-93/App-Deploy/static/js/runtime~main.00846a93.js"
+  },
+  {
+    "revision": "e26fd1335b099db7881f",
+    "url": "/Sushmitha-93/App-Deploy/static/js/main.d611c7b6.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Sushmitha-93/App-Deploy/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e26fd1335b099db7881f",
+    "url": "/Sushmitha-93/App-Deploy/static/css/main.665f4e07.chunk.css"
+  },
+  {
+    "revision": "1b4982734130ee0ee3dd4762b6290840",
+    "url": "/Sushmitha-93/App-Deploy/index.html"
+  }
+];
